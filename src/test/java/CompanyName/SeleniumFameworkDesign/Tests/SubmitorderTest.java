6a5a8c2f56abef551b72b632e2c73c7fc19359d1
devendrakotiya <devendrakotiya01@gmail.com>
2025-02-14 16:28:31 +0530

    added getscreenshot method in submitorder class

--- src/test/java/CompanyName/SeleniumFameworkDesign/Tests/SubmitorderTest.java
+++ src/test/java/CompanyName/SeleniumFameworkDesign/Tests/SubmitorderTest.java
@@ -1,11 +1,15 @@
 package CompanyName.SeleniumFameworkDesign.Tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import org.openqa.selenium.interactions.Actions;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -50,6 +54,13 @@ public class SubmitorderTest extends BaseTest {
 	
 	}
 	
+	public String getScreenshot(String testCaseName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file= new File(System.getProperty("user.dir")+"//reports//"+ testCaseName+ ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir")+"//reports//"+ testCaseName+ ".png";
+	}
 	@DataProvider
 	public Object[][] getData() throws IOException
 	{
